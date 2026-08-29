d81b6a6c440620b952f0da8907d2c653ca6310b8
kensyouen <kenji.62.722@gmail.com>
2026-08-30 02:33:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # === ページ設定 ===
-st.set_page_config(page_title="Family Wealth Compass", page_icon="🧭", layout="wide")
+st.set_page_config(page_title="Family Wealth Compass", page_icon="💲", layout="wide")
 
 # ==========================================
 # 🔒 パスワード認証機能
